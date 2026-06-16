e2c328a16ae1829128ced62ae310e53b730aac9d
mouhamed <mouhamed.sow7@unchk.edu.sn>
2026-06-16 01:33:05 +0000

    adding digitalesf.com in cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(
   cors({
     origin: [
       "https://mouhamedsow.digitalesf.com",
+      "https://digitalesf.com",
       "http://127.0.0.1:5500",
       "http://127.0.0.1:5501",
       "http://localhost:5500",
